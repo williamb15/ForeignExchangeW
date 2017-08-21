70cb955ae0c64e19b9594dea607a1667c2ce7ecb
williamb.a <williamb.a@hotmail.com>
2017-08-20 20:47:17 -0500

    3 video ok

---
+++ ForeignExchangeW/ForeignExchangeW/Models/Rates.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForeignExchangeW.Models
+{
+    public class Rates
+    {
+        public int RateId { get; set; }
+
+        public string Code { get; set; }
+
+        public double TaxRate { get; set; }
+
+        public string Name { get; set; }
+    }
+}
+    
--- ForeignExchangeW/ForeignExchangeW/ViewModels/MainViewModel.cs
+++ ForeignExchangeW/ForeignExchangeW/ViewModels/MainViewModel.cs
@@ -2,7 +2,53 @@
 
 namespace ForeignExchangeW.ViewModels
 {
+    using GalaSoft.MvvmLight.Command;
+    using Models;
+    using System.Collections.ObjectModel;
+    using System.Windows.Input;
+    using System;
+
+
     public class MainViewModel
     {
+
+    #region Properties
+
+    public string Amount { get; set; }
+
+    public ObservableCollection<Rates> Rates { get; set; }
+
+    public Rates SourceRate { get; set; }
+
+    public Rates TargetRate { get; set; }
+
+    public bool IsRunning { get; set; }
+
+    public bool IsEnabled { get; set; }
+
+    public string Result { get; set; }
+
+    #endregion
+
+   
+
+    #region Commands
+    public ICommand ConvertCommand
+    {
+        get
+        {
+            return new RelayCommand(Convert);
+        }
+        
     }
+
+    void Convert()
+    {
+        throw new NotImplementedException();
+    }
+
+        #endregion
+
+    }
+
 }
