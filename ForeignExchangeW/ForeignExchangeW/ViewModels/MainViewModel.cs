2f1f44eace9059b28aa610838cf0fd0fb7d6b8ed
williamb.a <williamb.a@hotmail.com>
2017-08-20 22:40:04 -0500

    completo video 5 ok

--- ForeignExchangeW/ForeignExchangeW/ViewModels/MainViewModel.cs
+++ ForeignExchangeW/ForeignExchangeW/ViewModels/MainViewModel.cs
@@ -7,48 +7,220 @@ namespace ForeignExchangeW.ViewModels
     using System.Collections.ObjectModel;
     using System.Windows.Input;
     using System;
+    using System.ComponentModel;
+    using System.Net.Http;
+    using Newtonsoft.Json;
+    using System.Collections.Generic;
+    using Xamarin.Forms;
 
-
-    public class MainViewModel
+    public class MainViewModel : INotifyPropertyChanged
     {
 
-    #region Properties
+        #region Events
 
-    public string Amount { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
 
-    public ObservableCollection<Rates> Rates { get; set; }
+        #endregion
 
-    public Rates SourceRate { get; set; }
+        #region Attributes
 
-    public Rates TargetRate { get; set; }
+        bool _IsRunning;
+        bool _IsEnabled;
+        string _Results;
+        ObservableCollection<Rates> _rates;
 
-    public bool IsRunning { get; set; }
+        #endregion
 
-    public bool IsEnabled { get; set; }
+        #region Properties
 
-    public string Result { get; set; }
+        public string Amount { get; set; }
 
-    #endregion
+        public ObservableCollection<Rates> Rates
+        {
+            get
+            {
+                return _rates;
 
-   
+            }
+            set
+            {
+                if (_rates != value)
+                {
+                    _rates = value;
+                    PropertyChanged?.Invoke(
+                        this,
+                        new PropertyChangedEventArgs(nameof(Rates)));
+                }
+            }
+        }
 
-    #region Commands
-    public ICommand ConvertCommand
-    {
+        public Rates SourceRate { get; set; }
+
+        public Rates TargetRate { get; set; }
+
+        public bool IsRunning
+        {
+            get
+            {
+                return _IsRunning;
+              
+            }
+            set
+            {
+                if(_IsRunning != value)
+                {
+                    _IsRunning = value;
+                    PropertyChanged?.Invoke(
+                        this,
+                        new PropertyChangedEventArgs(nameof(IsRunning)));
+                }
+            }
+        }
+
+        public bool IsEnabled
+        {
+            get
+            {
+                return _IsEnabled;
+
+            }
+            set
+            {
+                if (_IsEnabled != value)
+                {
+                    _IsEnabled = value;
+                    PropertyChanged?.Invoke(
+                        this,
+                        new PropertyChangedEventArgs(nameof(IsEnabled)));
+                }
+            }
+        }
+
+        public string Result
+        {
+            get
+            {
+                return _Results;
+
+            }
+            set
+            {
+                if (_Results != value)
+                {
+                    _Results = value;
+                    PropertyChanged?.Invoke(
+                        this,
+                        new PropertyChangedEventArgs(nameof(Result)));
+                }
+            }
+        }
+
+         #endregion
+
+        #region Commands
+        public ICommand ConvertCommand
+        {
         get
         {
             return new RelayCommand(Convert);
         }
         
-    }
+        }
 
-    void Convert()
-    {
-        throw new NotImplementedException();
-    }
+         async void Convert()
+         {
+            if(string.IsNullOrEmpty(Amount))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter a value in amount",
+                    "Accept");
+                return;
+            }
+            decimal amount = 0;
+            if(!decimal.TryParse (Amount, out amount))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter a numeric value in amount",
+                    "Accept");
+                return;
+            }
+            if (SourceRate == null)
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must select a source rate",
+                    "Accept");
+                return;
+            }
+            if (TargetRate == null)
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must select a target rate",
+                    "Accept");
+                return;
+            }
 
-        #endregion
+            var amountConverted = amount / 
+                                  (decimal)SourceRate.TaxRate * 
+                                  (decimal)TargetRate.TaxRate;
 
+            Result = string.Format("{0}{1:C2} = {2}{3:C2}",
+                SourceRate.Code, 
+                amount, 
+                TargetRate.Code, 
+                amountConverted);
+        }
+
+         #endregion
+
+        #region Constructors
+
+          public MainViewModel()
+          {
+            LoadRates();
+           }
+
+         #endregion
+
+        #region Methods
+
+        async void LoadRates ()
+        {
+            IsRunning = true;
+            Result = "Loading Rates..";
+
+            try
+            {
+                var client = new HttpClient();
+                client.BaseAddress = new 
+                    Uri("http://apiexchangerates.azurewebsites.net");
+                var controller = "/api/Rates";
+                var response = await client.GetAsync(controller);
+                var result = await response.Content.ReadAsStringAsync();
+                if(!response.IsSuccessStatusCode)
+                {
+                    IsRunning = false;
+                    Result = result;
+                }
+                var rates = JsonConvert.DeserializeObject<List<Rates>>(result);
+                Rates = new ObservableCollection<Models.Rates>(rates);
+                IsRunning = false;
+                IsEnabled = true;
+                Result = "Ready to convet!";
+            }
+            catch (Exception ex)
+            {
+                IsRunning = false;
+                Result = ex.Message; 
+                throw;
+            }
+
+        }
+
+     #endregion
     }
 
 }
