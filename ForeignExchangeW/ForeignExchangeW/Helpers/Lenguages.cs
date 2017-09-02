35ef26c43db43e703242e5e1afdaadf8964c27f8
williamb.a <williamb.a@hotmail.com>
2017-09-02 18:23:59 -0500

    idiomas ok

---
+++ ForeignExchangeW/ForeignExchangeW/Helpers/Lenguages.cs
@@ -0,0 +1,89 @@
+﻿
+
+namespace ForeignExchangeW.Helpers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Xamarin.Forms;
+    using Interfaces;
+    using Resources;
+
+    public static class Lenguages
+    {
+        static Lenguages()
+        {
+            var ci = DependencyService.Get<ILocalize>().GetCurrentCultureInfo();
+            Resource.Culture = ci;
+            DependencyService.Get<ILocalize>().SetLocale(ci);
+        }
+
+        public static string Title
+        {
+            get { return Resource.Title; }
+        }
+        public static string AmountValidation
+        {
+            get { return Resource.AmountValidation; }
+        }
+        public static string Error
+        {
+            get { return Resource.Error; }
+        }
+        public static string Accept
+        {
+            get { return Resource.Accept; }
+        }
+        public static string AmountNumericValidation
+        {
+            get { return Resource.AmountNumericValidation; }
+        }
+        public static string SourceRateValidation
+        {
+            get { return Resource.SourceRateValidation; }
+        }
+        public static string TargetRateValidation
+        {
+            get { return Resource.TargetRateValidation; }
+        }
+        public static string AmountLabel
+        {
+            get { return Resource.AmountLabel; }
+        }
+        public static string AmountPlaceHolder
+        {
+            get { return Resource.AmountPlaceHolder; }
+        }
+        public static string Convert
+        {
+            get { return Resource.Convert; }
+        }
+        public static string SourceRateTitle
+        {
+            get { return Resource.SourceRateTitle; }
+        }
+        public static string SourceRateLabel
+        {
+            get { return Resource.SourceRateLabel; }
+        }
+        public static string TargetRateTitle
+        {
+            get { return Resource.TargetRateTitle; }
+        }
+        public static string TargetRateLabel
+        {
+            get { return Resource.TargetRateLabel; }
+        }
+        public static string Loading
+        {
+            get { return Resource.Loading; }
+        }
+        public static string Ready
+        {
+            get { return Resource.Ready; }
+        }
+    }
+
+}
---
+++ ForeignExchangeW/ForeignExchangeW/Interfaces/ILocalize.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace ForeignExchangeW.Interfaces
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Globalization;
+
+    public interface ILocalize
+    {
+        CultureInfo GetCurrentCultureInfo();
+
+        void SetLocale(CultureInfo ci);
+    }
+
+}
